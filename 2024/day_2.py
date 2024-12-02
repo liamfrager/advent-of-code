b48562c899883e6182ff18778d3509d9626910bf
Liam Frager <liam.frager@gmail.com>
2024-12-02 10:47:51 -0600

    day 2 part 1

--- 2024/day_2.py
+++ 2024/day_2.py
@@ -4,7 +4,18 @@ puzzle = get_puzzle('2')
 
 
 def part1():
-    return
+    unsafe_reports = 0
+    for report in puzzle:
+        report = [int(num) for num in report.split(' ')]
+        direction = 1 if report[len(report) - 1] > report[0] else -1
+        for i in range(0, len(report) - 1):
+            level = report[i]
+            next = report[i + 1]
+            if not 1 <= (next - level) * direction <= 3:
+                unsafe_reports += 1
+                break
+    safe_reports = len(puzzle) - unsafe_reports
+    return safe_reports
 
 
 def part2():
