067f60df0a2daac0b9500f552211cd59c751deb2
Liam Frager <liam.frager@gmail.com>
2024-12-02 16:07:46 -0600

    day 2 part 2

--- 2024/day_2.py
+++ 2024/day_2.py
@@ -3,6 +3,16 @@ from file_reader_util import get_puzzle
 puzzle = get_puzzle('2')
 
 
+def is_safe_report(report: list) -> bool:
+    direction = 1 if report[len(report) - 1] > report[0] else -1
+    for i in range(0, len(report) - 1):
+        level = report[i]
+        next = report[i + 1]
+        if not 1 <= (next - level) * direction <= 3:
+            return False
+    return True
+
+
 def part1():
     unsafe_reports = 0
     for report in puzzle:
@@ -19,7 +29,19 @@ def part1():
 
 
 def part2():
-    return
+    safe_reports = 0
+    for report in puzzle:
+        report = [int(num) for num in report.split(' ')]
+        if not is_safe_report(report):
+            for j in range(len(report)):
+                fix_report = report[:]
+                fix_report.pop(j)
+                if is_safe_report(fix_report):
+                    safe_reports += 1
+                    break
+        else:
+            safe_reports += 1
+    return safe_reports
 
 
 print(f'Part 1: {part1()}')
