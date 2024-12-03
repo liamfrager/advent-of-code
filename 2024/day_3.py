f57066fc5a4ec35d892cbad6871d3e6cb85981e2
Liam Frager <liam.frager@gmail.com>
2024-12-03 09:09:08 -0600

    day 3 part 1

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -4,7 +4,22 @@ puzzle = get_puzzle('3')
 
 
 def part1():
-    return
+    sum = 0
+    for line in puzzle:
+        lines = line.split('mul(')
+        for func in lines:
+            if ')' in func:
+                end = func.index(')')
+                if ' ' not in func[:end]:
+                    nums = func[:end].split(',')
+                    if len(nums) == 2:
+                        try:
+                            num1 = int(nums[0])
+                            num2 = int(nums[1])
+                            sum += num1 * num2
+                        except Exception as e:
+                            continue
+    return sum
 
 
 def part2():
