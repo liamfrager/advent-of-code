55c9509ef8aa124f893b55e08c5da57349f1f708
Liam Frager <liam.frager@gmail.com>
2024-12-03 09:46:36 -0600

    day 3 part 2

--- 2024/day_3.py
+++ 2024/day_3.py
@@ -3,6 +3,19 @@ from file_reader_util import get_puzzle
 puzzle = get_puzzle('3')
 
 
+def count_muls(input: str):
+    sum = 0
+    lines = input.split('mul(')
+    for func in lines:
+        if ')' in func:
+            end = func.index(')')
+            if ' ' not in func[:end]:
+                nums = func[:end].split(',')
+                if len(nums) == 2 and all([num.isnumeric() for num in nums]):
+                    sum += int(nums[0]) * int(nums[1])
+    return sum
+
+
 def part1():
     sum = 0
     for line in puzzle:
@@ -23,7 +36,14 @@ def part1():
 
 
 def part2():
-    return
+    sum = 0
+    lines = ''.join(puzzle).split("don't()")
+    sum += count_muls(lines[0])
+    for funcs in lines[1:]:
+        if "do()" in funcs:
+            do_funcs = funcs[funcs.index("do()"):]
+            sum += count_muls(do_funcs)
+    return sum
 
 
 print(f'Part 1: {part1()}')
