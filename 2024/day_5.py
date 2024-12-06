aadcc434b48ee92e33fa897c2685c80bdaa494b2
Liam Frager <liam.frager@gmail.com>
2024-12-05 19:59:49 -0500

    day 5 part 1

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -5,8 +5,22 @@ updates = [[int(num) for num in update.split(',')]
            for update in get_puzzle('5B')]
 
 
+def is_valid_update(update: list[int]) -> bool:
+    for rule in rules:
+        x, y = rule
+        if x in update and y in update:
+            if update.index(x) > update.index(y):
+                return False
+    return True
+
+
 def part1():
-    return
+    sum = 0
+    for update in updates:
+        if is_valid_update(update):
+            middle = (len(update) - 1) // 2
+            sum += update[middle]
+    return sum
 
 
 def part2():
