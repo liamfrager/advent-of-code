6bd89f2bbdc296b6234b828bf360f8b729831956
Liam Frager <liam.frager@gmail.com>
2024-12-06 18:45:58 -0500

    day 6 part 2

--- 2024/day_6.py
+++ 2024/day_6.py
@@ -1,8 +1,17 @@
 from file_reader_util import get_puzzle
-import itertools
 
 puzzle = get_puzzle('6')
-directions = itertools.cycle([(-1, 0), (0, 1), (1, 0), (0, -1)])
+loop_check_counter = 0
+
+
+def inc_loop():
+    global loop_check_counter
+    loop_check_counter += 1
+
+
+def turn_right(vector):
+    vectors = [(-1, 0), (0, 1), (1, 0), (0, -1)]
+    return vectors[(vectors.index(vector) + 1) % 4]
 
 
 def get_start_position():
@@ -21,19 +30,46 @@ def is_in_area(row, col):
     ])
 
 
+def can_move(row, col, vector, obstacle=None):
+    x, y = vector
+    new_row, new_col = row + x, col + y
+    if is_in_area(new_row, new_col) and puzzle[new_row][new_col] != '#' and (new_row, new_col) != obstacle:
+        return True
+    return False
+
+
 def move(row, col, vector):
     x, y = vector
-    new_row = row + x
-    new_col = col + y
+    new_row, new_col = row + x, col + y
     if puzzle[new_row][new_col] != '#':
         return new_row, new_col, vector
-    new_vector = next(directions)
+    new_vector = turn_right(vector)
     return row, col, new_vector
+
+
+def results_in_loop(r, c, v, o) -> bool:
+    been_here = set()
+    map = list(puzzle)
+    while True:
+        x, y = v
+        nr, nc = r + x, c + y
+        if can_move(r, c, v, o):
+            r, c = nr, nc
+            # map[r] = map[r][:c] + 'X' + map[r][c + 1:]
+        elif not is_in_area(nr, nc):
+            return False
+        elif (r, c, v) in been_here:
+            return True
+        else:
+            been_here.add((r, c, v))
+            v = turn_right(v)
+            # for i in map:
+            #     print(i)
 
 
 def part1():
     vector = (-1, 0)
-    map = puzzle
+    map = list(puzzle)
     row, col = get_start_position()
     while is_in_area(row, col):
         map[row] = map[row][:col] + 'X' + map[row][col + 1:]
@@ -42,8 +78,29 @@ def part1():
 
 
 def part2():
-    return
+    infinite_loops = set()
+    start_vector = (-1, 0)
+    start_row, start_col = get_start_position()
+    vector = tuple(start_vector)
+    row, col = int(start_row), int(start_col)
+    counter = 0
+    while is_in_area(row, col):
+        print(f'{counter}: {row},{col} + {vector}')
+        x, y = vector
+        new_row, new_col = row + x, col + y
+        if can_move(row, col, vector):  # if can place obstacle in front
+            obstacle = (new_row, new_col)
+            if results_in_loop(start_row, start_col, start_vector, obstacle):
+                infinite_loops.add((new_row, new_col))
+                print(f'found loop: {len(infinite_loops)}')
+            row, col = new_row, new_col
+        elif is_in_area(new_row, new_col):  # if hit obstacle
+            vector = turn_right(vector)
+        else:  # if gone of the edge
+            return len(infinite_loops)
+        counter += 1
 
 
 print(f'Part 1: {part1()}')
 print(f'Part 2: {part2()}')
+print(f'global loop counter: {loop_check_counter}')
