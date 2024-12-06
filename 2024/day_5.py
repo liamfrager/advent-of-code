dc2a3462d83c6a8319ebe0ab81401b400087a6b1
Liam Frager <liam.frager@gmail.com>
2024-12-05 20:21:26 -0500

    day 5 part 2

--- 2024/day_5.py
+++ 2024/day_5.py
@@ -14,6 +14,16 @@ def is_valid_update(update: list[int]) -> bool:
     return True
 
 
+def order_update(update: list[int]) -> list[int]:
+    applicable_rules = [rule for rule in rules if rule[0]
+                        in update and rule[1] in update]
+    ordered_update = [0] * len(update)
+    for num in update:
+        i = len([rule for rule in applicable_rules if rule[1] == num])
+        ordered_update[i] = num
+    return ordered_update
+
+
 def part1():
     sum = 0
     for update in updates:
@@ -24,7 +34,13 @@ def part1():
 
 
 def part2():
-    return
+    sum = 0
+    for update in updates:
+        if not is_valid_update(update):
+            ordered_update = order_update(update)
+            middle = (len(ordered_update) - 1) // 2
+            sum += ordered_update[middle]
+    return sum
 
 
 print(f'Part 1: {part1()}')
