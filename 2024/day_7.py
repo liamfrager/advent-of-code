193b9e2dcbdc1b0072df748d87b534b82f30013f
Liam Frager <liam.frager@gmail.com>
2024-12-07 09:44:39 -0500

    day 7 part 1

--- 2024/day_7.py
+++ 2024/day_7.py
@@ -1,10 +1,31 @@
 from file_reader_util import get_puzzle
+from itertools import product
 
 puzzle = get_puzzle('7')
 
 
 def part1():
-    return
+    calibration_result = 0
+    for equation in puzzle:
+        answer, numbers = equation.split(': ')
+        answer = int(answer)
+        numbers = [int(num) for num in numbers.split(' ')]
+
+        sequence = ['+', '*']
+        n = len(numbers) - 1
+        all_combinations = list(product(sequence, repeat=n))
+
+        for operations in all_combinations:
+            result = numbers[0]
+            for i in range(1, len(numbers)):
+                if operations[i - 1] == '+':
+                    result += numbers[i]
+                elif operations[i - 1] == '*':
+                    result *= numbers[i]
+            if result == answer:
+                calibration_result += answer
+                break
+    return calibration_result
 
 
 def part2():
