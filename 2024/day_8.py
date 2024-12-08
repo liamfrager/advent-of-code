d578489c92b105fa11d6b126c6d0ed03eabb5d05
Liam Frager <liam.frager@gmail.com>
2024-12-08 09:55:39 -0500

    day 8 part 2

--- 2024/day_8.py
+++ 2024/day_8.py
@@ -3,6 +3,20 @@ from file_reader_util import get_puzzle
 puzzle = get_puzzle('8')
 
 
+def get_all_antennae() -> dict[str, list[tuple[int]]]:
+    antennae: dict[str, list[tuple[int]]] = {}
+
+    for i in range(len(puzzle)):
+        for j in range(len(puzzle[0])):
+            char = puzzle[i][j]
+            if char != '.':
+                if char in antennae.keys():
+                    antennae[char].append((i, j))
+                else:
+                    antennae[char] = [(i, j)]
+    return antennae
+
+
 def is_in_area(coords: tuple[int]) -> bool:
     row, col = coords
     return all([
@@ -24,17 +38,7 @@ def get_antinode(node1, node2) -> tuple[int]:
 
 
 def part1():
-    antennae: dict[str, list[tuple[int]]] = {}
-
-    for i in range(len(puzzle)):
-        for j in range(len(puzzle[0])):
-            char = puzzle[i][j]
-            if char != '.':
-                if char in antennae.keys():
-                    antennae[char].append((i, j))
-                else:
-                    antennae[char] = [(i, j)]
-
+    antennae: dict[str, list[tuple[int]]] = get_all_antennae()
     antinodes = set()
 
     for key in antennae.keys():
@@ -50,7 +54,25 @@ def part1():
 
 
 def part2():
-    return
+    antennae: dict[str, list[tuple[int]]] = get_all_antennae()
+    antinodes = set()
+
+    for key in antennae.keys():
+        coords = antennae[key]
+        for i in range(len(coords)):
+            if len(coords) > 1:
+                antinodes.add(coords[i])
+            for j in range(len(coords)):
+                if i != j:
+                    antinode = get_antinode(coords[i], coords[j])
+                    prev_node = coords[j]
+                    while is_in_area(antinode):
+                        antinodes.add(antinode)
+                        new_node = get_antinode(prev_node, antinode)
+                        prev_node = antinode
+                        antinode = new_node
+
+    return len(antinodes)
 
 
 print(f'Part 1: {part1()}')
