23940cbd0e09ed64c5b9c00b04e7d3e2cecfd038
Liam Frager <liam.frager@gmail.com>
2024-12-09 08:56:58 -0500

    day 9 init

---
+++ 2024/day_9.py
@@ -0,0 +1,15 @@
+from file_reader_util import get_puzzle
+
+puzzle = get_puzzle('9')
+
+
+def part1():
+    return
+
+
+def part2():
+    return
+
+
+print(f'Part 1: {part1()}')
+print(f'Part 2: {part2()}')
