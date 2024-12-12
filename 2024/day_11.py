3d94456208f01306cf1fd62394473328ce55d173
Liam Frager <liam.frager@gmail.com>
2024-12-11 22:24:06 -0500

    day 11 part 1

--- 2024/day_11.py
+++ 2024/day_11.py
@@ -3,8 +3,21 @@ from file_reader_util import get_puzzle
 puzzle = [int(cell) for cell in get_puzzle('11')[0].split(' ')]
 
 
+def alter_stone(stone: int):
+    if stone == 0:
+        return [1]
+    if len(str(stone)) % 2 == 0:
+        string = str(stone)
+        i = len(string) // 2
+        return [int(string[:i]), int(string[i:])]
+    return [stone * 2024]
+
+
 def part1():
-    return
+    stones = list(puzzle)
+    for _ in range(25):
+        stones = [item for stone in stones for item in alter_stone(stone)]
+    return len(stones)
 
 
 def part2():
