d2057e376cadb91b4f9fc4015aabe5f24a78b508
Liam Frager <liam.frager@gmail.com>
2024-12-11 22:10:08 -0500

    day 11 init

---
+++ 2024/day_11.py
@@ -0,0 +1,15 @@
+from file_reader_util import get_puzzle
+
+puzzle = [int(cell) for cell in get_puzzle('11')[0].split(' ')]
+
+
+def part1():
+    return
+
+
+def part2():
+    return
+
+
+print(f'Part 1: {part1()}')
+print(f'Part 2: {part2()}')
